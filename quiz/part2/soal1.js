952ade23211855df51c8c6e247ac28d55c41942f
Rafa raf <rasyidrafa28@gmail.com>
2025-03-29 16:29:02 +0700

    soal1

--- quiz/part2/soal1.js
+++ quiz/part2/soal1.js
@@ -1,39 +1,18 @@
-// Kamu diminta untuk memprogram suatu game sederhana, Proxytia namanya. 
-// Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), 
-// yaitu nama dan peran. Variabel peran harus memiliki isi data, 
-// bila nama kosong, pemain akan diberikan peringatan berupa "nama wajib diisi"
-// bila peran kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game". 
-// Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir. 
-// Tugas Anda adalah untuk membuat program yang mengecek isi variabel 
-// peran serta mengeluarkan respon sesuai isi variabel tersebut.
-//ada 4 jenis respons sesuai dengan 3 jenis peran yaitu
-//halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!
-//halo Tabib ${nama} , kamu akan membantu temanmu yang terluka
-//halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!
-//tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada
-//tips belajar penggunaan `` (backtick) pada javascript agar
-//mudah dalam memasukan variabel ke dalam string
-//tapi tanpa backtick juga ga masalah sih yg penting output sesuai
+//1. Let's Form a Sentence
 
-// algoritma
-//isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +.
+//  disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain
+//  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
+//   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
 
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
 
-let nama = "joko", peran = "Tabib";
-
-//code disini gunakan console.log untuk outputnya
-if (nama == "") {
-    console.log("nama wajib diisi")
-}else {
-    if (peran == "") {
-        console.log("Pilih Peranmu untuk memulai game")
-    }else{
-        if (peran == "ksatria") {
-            console.log(`halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!`)
-        }else if(peran == "Tabib") {
-            console.log(`halo Tabib ${nama} , kamu akan membantu temanmu yang terluka`)
-        }else if (peran == "penyihir"){
-            console.log(`halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!`)
-        }
-    }
-}
+//code here
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
