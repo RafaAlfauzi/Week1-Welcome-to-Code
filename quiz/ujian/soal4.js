6c0d72169e3947bcac9fd6a316319dd943e8a0e5
Rafa raf <rasyidrafa28@gmail.com>
2025-03-29 19:02:42 +0700

    penambahan semua soal ujian

---
+++ quiz/ujian/soal4.js
@@ -0,0 +1,18 @@
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. 
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+//   if x = o : true
+//   else : false
+
+function xo(str) {
+    // you can only write your code here!
+    return str.split('x').length === str.split('o').length; 
+    
+}
+  
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
